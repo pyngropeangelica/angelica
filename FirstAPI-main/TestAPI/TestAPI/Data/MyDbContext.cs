554465601e1812229a1df6ef36e1fc7ea7511ddb
pyngropeangelica <120253388+pyngropeangelica@users.noreply.github.com>
2023-05-02 14:47:51 +0530

    Add files via upload

---
+++ FirstAPI-main/TestAPI/TestAPI/Data/MyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestAPI.Models;
+
+namespace TestAPI.Data
+{
+    public class MyDbContext:DbContext
+    {
+        //public MyDbContext()
+        //{
+        //    Database.EnsureCreated();
+        //}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-C0H3C8Q\SQLEXPRESS;Database=APIDB;Trusted_Connection=True;");
+        }
+
+        public DbSet<Event> Events { get; set; }
+    }
+}
